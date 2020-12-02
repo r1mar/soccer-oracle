3a1b6d224ba1a864bc45b00ede53c991b239bead
richard-martens <richard.martens@outlook.com>
2020-12-02 21:21:24 +0100

    Update StartMenu.js

--- app/src/StartMenu.js
+++ app/src/StartMenu.js
@@ -1,15 +1,39 @@
 import React from "react";
 import "./style.css";
 import PageHeader from "./PageHeader";
+import Alert from "./Alert";
 
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      message: "",
+      errors = []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      let response = await fetch("/api");
+
+      this.setState({
+        message: await response.text()
+      });
+    } catch(e) {
+      this.setState({
+        errors: [e]
+      };
+    }
   }
 
   render() {
     return (
-      <PageHeader history={this.props.history} title="Startseite" />
+      <div>
+        <PageHeader history={this.props.history} title="Startseite" />
+        <h2>{this.state.message}</h2>
+        <Alert messages={this.state.errors} />
+      </div>
     );
   }
 }
