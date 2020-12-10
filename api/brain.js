950e6e6d470e03b22e4220931d75df3ec7f3a397
richard-martens <richard.martens@outlook.com>
2020-12-10 20:39:54 +0100

    Update brain.js

--- api/brain.js
+++ api/brain.js
@@ -19,22 +19,34 @@ exports.getPrediction = async function (matches, team1, team2) {
             const host = teams.map(team => team.TeamId === match.Team1.TeamId ? 1 : 0),
                   guest = teams.map(team => team.TeamId === match.Team2.TeamId ? 1 : 0),
                   matchResult = match.MatchResults.find(matchResult => matchResult.ResultTypeID === 2);
+
+            var winner = [0,0];
+            if(matchResult.PointsTeam1 > matchResult.PointsTeam2) {
+              winner[0] = 1;
+            }
+
+            if(matchResult.PointsTeam1 < matchResult.PointsTeam2) {
+              winner[1] = 1;
+            }
+
+            var goalsHost = [], goalsGuest = [];
+
+            do {
+              goalsHost.push(matchResult.PointsTeam1 === goalsHost.length + 1 || goalsHost.length === 10 ? 1 : 0;
+              goalsGuest.push(matchResult.PointsTeam2 === goalsGuest.length + 1 || goalsGuest.length === 10 ? 1 : 0;
+            } while(goalsHost.length < 10)
+
             return {
                 input: [...host, ...guest],
-                output: []
+                output: [...winner, ...goalsHost, ...goalsGuest]
             };
     });
     
-          input = []
+    net.train(trainData);
 
-    net.train([{ input: [0, 0], output: [0] },
-    { input: [1, 1], output: [0] }]);
+    const host = teams.map(team => team.TeamId === team1 ? 1 : 0),
+          guest = teams.map(team => team.TeamId === team2 ? 1 : 0);
 
-    return net.run([1, 0]);
-
-
-    net.train(trainingData);
-
-    net.run({ blue: 1 })
+    return net.run([...host, ...guest]);
 
 }
