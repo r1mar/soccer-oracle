3cc03fff5c00d50f14284d665ab9a12a192f9c0c
richard-martens <richard.martens@outlook.com>
2020-12-14 22:11:37 +0100

    Update mongo.js

--- api/mongo.js
+++ api/mongo.js
@@ -1,18 +1,29 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?w=majority";
+const client = new MongoClient(uri);
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+exports.sync = async () => {
+  try {
+    await client.connect();
 
-// Database Name
-const dbName = 'soccer-oracle';
+    const database = client.db("soccer-oracle");
+    const collection = database.collection("brains");
+    
+    // Query for a smartest brain that knows the latest matches
+    const query = { };
+    const options = {
+      // sort matched documents in descending order by change-date
+      sort: { "change-date": -1 },
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: { _id: 0, brain: 1, "change-date": 1 },
+    };
+    const brain = await collection.findOne(query, options);
+    // since this method returns the matched document, not a cursor, print it directly
+    
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
+  } finally {
+    await client.close();
+  }
+}
